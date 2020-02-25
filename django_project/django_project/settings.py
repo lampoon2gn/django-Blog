3203d42f8da387151e89c06ced3acbcce5fa354d
Maohan Sun <lampoon2gn@gmail.com>
2020-02-24 17:46:38 -0700

    + login/out/profile templates

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -123,4 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4' #! bootstrap version to use for crisp forms
+
+LOGIN_REDIRECT_URL = 'blog-home'#! redirect to blog after login
+
+LOGIN_URL = 'login'
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm #! form we created in forms.py, includes an email filed
+from django.contrib.auth.decorators import login_required #! decorator to declare view as logged in users only
 
 # Create your views here.
 def register(request):
@@ -11,12 +12,16 @@ def register(request):
       form.save()#* if check is passed, save user
       username = form.cleaned_data.get('username') #! username is in cleaned_data dictionary
       messages.success(request,f'Account created for {username}!')
-      return redirect('blog-home')
+      return redirect('login')
     else:
       messages.warning(request,f"Something's not right...Please check your form!")
   else:#! not POST request, display empty form
     form = UserRegisterForm()
   return render(request,'users/register.html',{'form':form})
+
+@login_required #! decorator to declare view as logged in users only
+def profile(request):
+  return render(request, 'users/profile.html')
 
   # message.debug
   # message.info
