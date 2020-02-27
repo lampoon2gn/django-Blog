9385850638e17dfa6a48a05c4cce4c848e4e1c79
Maohan Sun <lampoon2gn@gmail.com>
2020-02-27 11:45:46 -0700

    + create/update/delete post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post #. means its in the package
+from django.views.generic import (
+  ListView,
+  DetailView,
+  CreateView,
+  UpdateView,
+  DeleteView
+)
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin #! can't use decorator to set a class view to logged in only, so this mixin is used and passed to the class argument
 
 # Create your views here.
 
-#-------------dummy data-------------
-# posts=[
-#   {
-#     'author':'cole',
-#     'title':'this is title',
-#     'content':'this is content',
-#     'date_posted':'01/23/2020'
-#   },
-#   {
-#     'author':'jin',
-#     'title':'this is also title',
-#     'content':'this is also content',
-#     'date_posted':'04/05/2020'
-#   },
-# ]
+
 
 def home(request):
   #gets list of dicts, pass to render()
@@ -28,6 +23,53 @@ def home(request):
   }
 
   return render(request,'blog/home.html',context)#string is the file path inside the template folder
+
+class PostListView(ListView):#! class based view
+  model = Post
+  template_name = 'blog/home.html' #! <app>/<model>_<viewtype>.html is the django template naming convention
+  context_object_name = 'posts'
+  ordering = ['-date_posted'] #! show newest post first
+
+class PostDetailView(DetailView):#! class based view
+  model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):#! class based view
+  model = Post
+  fields = ['title','content']
+
+  def form_valid(self,form):
+    form.instance.author = self.request.user
+    messages.success(self.request,f'Posted made!')
+    return super().form_valid(form)
+  
+  
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):#! class based view
+  model = Post
+  fields = ['title','content']
+
+  def form_valid(self,form):
+    form.instance.author = self.request.user
+    messages.success(self.request,f'Posted modified!')
+    return super().form_valid(form)
+  def test_func(self):#! use UserPassesTestMixin to only allow the original author to edit the post
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):#! class based view
+  model = Post
+  success_url ='/'#! if deleted, redirect to home page
+
+  def test_func(self):#! use UserPassesTestMixin to only allow the original author to edit the post
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+  
+  
+
 
 def about(request):
   return render(request,'blog/about.html',{'title':'About'})
