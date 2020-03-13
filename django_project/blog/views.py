9bacd871b4d011c50a6df2f2f7ef15e2d3831d9d
Maohan Sun <lampoon2gn@gmail.com>
2020-03-12 19:13:52 -0600

    + pagination, user posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post #. means its in the package
 from django.views.generic import (
@@ -10,7 +10,7 @@ from django.views.generic import (
 )
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin #! can't use decorator to set a class view to logged in only, so this mixin is used and passed to the class argument
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -29,6 +29,17 @@ class PostListView(ListView):#! class based view
   template_name = 'blog/home.html' #! <app>/<model>_<viewtype>.html is the django template naming convention
   context_object_name = 'posts'
   ordering = ['-date_posted'] #! show newest post first
+  paginate_by = 5#! pagination, 2 post per page
+
+class UserPostListView(ListView):#! copy from previous class, display posts of a certain user
+  model = Post
+  template_name = 'blog/user_posts.html'#! <app>/<model>_<viewtype>.html is the django template naming convention
+  context_object_name = 'posts'
+  paginate_by = 5#! pagination, 2 post per page
+
+  def get_queryset(self):
+    user = get_object_or_404(User,username = self.kwargs.get('username'))
+    return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):#! class based view
   model = Post
